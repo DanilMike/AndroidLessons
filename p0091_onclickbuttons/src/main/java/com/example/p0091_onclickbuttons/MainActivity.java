aba6cff0f5dd89caf521503a2f945fc51602d7cc
DanilMike <danil.mike@gmail.com>
2022-06-27 13:25:02 +0300

    Initial commit

---
+++ p0091_onclickbuttons/src/main/java/com/example/p0091_onclickbuttons/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.p0091_onclickbuttons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // найдем View-элементы
+        TextView tvOut = (TextView) findViewById(R.id.tvOut);
+        Button btnOk = (Button) findViewById(R.id.btnOk);
+        Button btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        View.OnClickListener oclBtnOk = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                tvOut.setText("Нажата кнопка ОК");
+            }
+        };
+
+        btnOk.setOnClickListener(oclBtnOk);
+
+        View.OnClickListener oclBtnCancel = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Меняем текст в TextView (tvOut)
+                tvOut.setText("Нажата кнопка Cancel");
+            }
+        };
+
+        btnCancel.setOnClickListener(oclBtnCancel);
+    }
+}
---
+++ p0211twoactivity/src/main/java/com/example/p0211twoactivity/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.p0211twoactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnActTwo;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        btnActTwo = (Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnActTwo:
+                // TODO Call second activity
+                Intent intent = new Intent(this, activity_two.class);
+                startActivity(intent);
+                break;
+
+                default:
+                break;
+        }
+    }
+}
